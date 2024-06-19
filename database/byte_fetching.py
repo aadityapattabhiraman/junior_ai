616dcf78df9aa87e0e403be01615df0f419c1505
aadityapattabhiraman <aadityapattabhiraman47@proton.me>
2024-06-19 03:25:31 +0000

    byte_fetching.py

--- database/byte_fetching.py
+++ database/byte_fetching.py
@@ -1,24 +1,22 @@
 #!/usr/bin/env python3
 
-from azure.storage.blob import BlobServiceClient
+import boto3
 
 
-connection_string = 'your_connection_string'
-container_name = 'your_container_name'
-blob_name = 'your_blob_name'
-output_file_path = 'output_file.txt'
+s3 = boto4.client("s3")
+bucket = ""
+kwy = ""
 
-start_range = 0
-end_range = 99
+brange = ["bytes=0-99", "bytes=100-199", "bytes=200-299"]
 
-blob_service_client = BlobServiceClient.from_connection_string(connection_string)
+parts = []
 
-blob_client = blob_service_client.get_blob_client(container_name, blob_name)
+for byte_range in byte_ranges:
 
-try:
-    with open(output_file_path, "wb") as my_blob:
-        blob_data = blob_client.download_blob(range=(start_range, end_range))
-        my_blob.write(blob_data.readall())
-    print(f"Byte range {start_range}-{end_range} of blob '{blob_name}' downloaded to '{output_file_path}'")
-except Exception as e:
-    print(f"Error downloading blob range: {e}")
+    response = s3.get_object(Bucket=bucket, Key=key, Range=brange)
+    parts.append(response["Body"].read())
+
+complete_file = b"".join(parts)
+
+with open("output_file", "wb") as f:
+    f.write(complete_file)
